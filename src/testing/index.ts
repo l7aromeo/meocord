8ef95d0bc10ba94d449f4c2d4c540450295628f3
L A Romeo <ukasyahrz@outlook.com>
2026-04-12 12:19:26 +0700

    feat(testing): export createMockInteraction, createChatInputOptions, DeepMocked

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -6,3 +6,6 @@
 
 export { MeoCordTestingModule, TestingModule, TestingModuleBuilder } from './meocord-testing-module.js'
 export type { Provider, ValueProvider, ClassProvider, TestingModuleOptions } from './meocord-testing-module.js'
+
+export { createMockInteraction, createChatInputOptions } from './mock-interaction.js'
+export type { DeepMocked, ChatInputOptions } from './mock-interaction.js'
